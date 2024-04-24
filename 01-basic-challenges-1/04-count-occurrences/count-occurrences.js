641a09cccfdf76d1ae04edf80eaf1a3260c0d8d6
Jan Maaß <82001155+jantillidie@users.noreply.github.com>
2024-04-24 22:49:19 +0200

    Add solution 01 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+// function countOccurrences(str, char) {
+//   let occurrences = 0
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (char === str[i]) {
+//       occurrences++
+//     }
+//   }
+
+//   return occurrences
+// }
+
+const countOccurrences = (str, char) => str.split(char).length -1
 
 module.exports = countOccurrences;
