bfd6478f76b1adfda9031f6071d6d3e460a025cd
Jan Maaß <82001155+jantillidie@users.noreply.github.com>
2024-04-25 16:29:59 +0200

    Add solution 01 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+  let maxNumber = [0]
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i]
+    }
+  }
+  return maxNumber
+}
 
 module.exports = findMaxNumber;
